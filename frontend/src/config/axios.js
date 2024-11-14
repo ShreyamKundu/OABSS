36e93071fc4043cdd8aaf9cf5ea6dd075dcf4411
Shrutika Jha <shrutikajha2603@gmail.com>
2024-11-15 01:25:44 +0530

    Update axios.js

--- frontend/src/config/axios.js
+++ frontend/src/config/axios.js
@@ -1,4 +1,4 @@
 import axios from 'axios'
-const REACT_APP_BASE_URL="https://oabss.vercel.app/?vercelToolbarCode=0cypGHpFq6bFpuc" ;
+const REACT_APP_BASE_URL="https://oabss.vercel.app/" ;
 
 export const axiosi=axios.create({withCredentials:true,baseURL:REACT_APP_BASE_URL})
