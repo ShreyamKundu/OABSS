203e128b33cf3205eb70a560ce0b21ed250db9b1
Shrutika Jha <shrutikajha2603@gmail.com>
2024-11-15 12:01:09 +0530

    Update index.js

--- backend/index.js
+++ backend/index.js
@@ -123,5 +123,5 @@ server.get("/", (req, res) => {
 
 // Start Server
 server.listen(8000, () => {
-  console.log('Server [STARTED] ~ https://oabss.vercel.app/?vercelToolbarCode=0cypGHpFq6bFpuc');
+  console.log('Server [STARTED] ~ https://oabss.vercel.app/');
 });
