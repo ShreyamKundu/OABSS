6d57b08443bc33a87102b07d04144fd9f2def1d4
Shrutika Jha <shrutikajha2603@gmail.com>
2024-11-16 13:00:38 +0000

    Revert "Update axios.js"

--- frontend/src/config/axios.js
+++ frontend/src/config/axios.js
@@ -1,4 +1,4 @@
 import axios from 'axios'
-const REACT_APP_BASE_URL="https://oabss-nl7b.onrender.com" ;
+const REACT_APP_BASE_URL="https://oabss.onrender.com" ;
 
 export const axiosi=axios.create({withCredentials:true,baseURL:REACT_APP_BASE_URL})
